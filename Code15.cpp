3fcf173bd58042ba7534d198e909a7863b1a9c20
RACHIT6 <89304686+RACHIT6@users.noreply.github.com>
2021-10-29 21:45:14 +0530

    Add files via upload

---
+++ Code15.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n, count = 0, rem;
+
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    while (n != 0)
+    {
+        rem = n%10;
+        count++;
+        n /= 10;
+    }
+    
+    cout<<"The number has "<<count<<" digits";
+
+    return 0;
+}
